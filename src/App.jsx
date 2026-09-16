9c19122e8564b03eac642c3848593173d802ee60
Webcreator-tech <creatorconnectin04@gmail.com>
2026-09-16 17:13:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
 } from "@react-three/drei";
 import { useEffect, useMemo, useRef, useState } from "react";
 import * as THREE from "three";
+import "./App.css";
 
 function Flat() {
   const { scene } = useGLTF("/models/flat.glb");
